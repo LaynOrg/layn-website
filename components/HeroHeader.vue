af78d88dee250637fc9f9c94687b67e159755b77
yali <tangozyusufali@gmail.com>
2023-12-07 19:18:06 +0300

    intl: hero header

--- components/HeroHeader.vue
+++ components/HeroHeader.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="mt-40 sm:mt-52 justify-center grid text-center">
         <h1 class="text-5xl font-bold mb-4">
-            We build your dreams
+            {{ $t('heroHeader.title') }}
         </h1>
         <p class="text-gray-500 font-semibold dark:text-gray-400 text-2xl">
-            Build your dreams with our solutions or contact us for your custom needs.
+            {{ $t('heroHeader.description') }}
         </p>
         <div class="flex flex-wrap justify-center gap-4 mt-10">
             <UButton color="black" size="xl">
-                See Solutions
+                {{ $t('heroHeader.solutions') }}
             </UButton>
             <UButton size="xl">
-                Contact Us
+                {{ $t('heroHeader.contact') }}
             </UButton>
         </div>
     </div>
--- i18n.config.ts
+++ i18n.config.ts
@@ -4,9 +4,23 @@ export default defineI18nConfig(() => ({
   messages: {
     tr: {
       headerLinks: ["Çözümler", "Takım", "İletişim"],
+      heroHeader: {
+        title: "Hayallerinizi inşa ediyoruz",
+        description:
+          "Çözümlerimizle hayallerinizi inşa edin veya özel ihtiyaçlarınız için bizimle iletişime geçin.",
+        solutions: "Çözümleri Görün",
+        contact: "İletişime Geçin",
+      },
     },
     en: {
       headerLinks: ["Solutions", "Team", "Contact"],
+      heroHeader: {
+        title: "We build your dreams",
+        description:
+          "Build your dreams with our solutions or contact us for your custom needs.",
+        solutions: "See Solutions",
+        contact: "Contact Us",
+      },
     },
   },
 }));
