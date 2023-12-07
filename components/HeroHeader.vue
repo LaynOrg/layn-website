4171d0836a6a71c6a8f9e972bd35489f303e57d7
yali <tangozyusufali@gmail.com>
2023-12-07 18:59:33 +0300

    feat: hero header

---
+++ components/HeroHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="mt-40 sm:mt-52 justify-center grid text-center">
+        <h1 class="text-5xl font-bold mb-4">
+            We build your dreams
+        </h1>
+        <p class="text-gray-500 font-semibold dark:text-gray-400 text-2xl">
+            Build your dreams with our solutions or contact us for your custom needs.
+        </p>
+        <div class="flex flex-wrap justify-center gap-4 mt-10">
+            <UButton color="black" size="xl">
+                See Solutions
+            </UButton>
+            <UButton size="xl">
+                Contact Us
+            </UButton>
+        </div>
+    </div>
+</template>
