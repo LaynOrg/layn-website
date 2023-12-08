3e5a115310b5445da6ccd959a37fd5c27b4f74d4
yali <tangozyusufali@gmail.com>
2023-12-08 17:16:49 +0300

    style: update contact card

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 const config = useRuntimeConfig()
 const emailHref = computed(() => `mailto:${config.public.emailAddress}`)
+
+const ui = {
+    base: "hover:shadow-xl transition-all duration-300 hover:scale-[1.02] ring-0",
+}
+
 </script>
 
 <template>
@@ -10,7 +15,7 @@ const emailHref = computed(() => `mailto:${config.public.emailAddress}`)
             {{ $t("contact.description") }}
         </p>
         <div class="grid grid-cols-1 sm:grid-cols-2 gap-5 text-white">
-            <UCard class="bg-[#7289da] dark:bg-[#7289da]" as="a" :href="config.public.discordUrl" target="_blank">
+            <UCard class="bg-[#7289da] dark:bg-[#7289da]" as="a" :href="config.public.discordUrl" target="_blank" :ui="ui">
                 <div class="flex items-center gap-5 mb-4">
                     <UIcon name="i-simple-icons-discord" class="text-4xl" />
                     <h1 class="text-2xl font-bold">Discord</h1>
@@ -19,7 +24,7 @@ const emailHref = computed(() => `mailto:${config.public.emailAddress}`)
                     {{ $t("contact.discordDescription") }}
                 </p>
             </UCard>
-            <UCard class="bg-red-500 dark:bg-red-500" as="a" :href="emailHref" target="_blank">
+            <UCard class="bg-red-500 dark:bg-red-500" as="a" :href="emailHref" target="_blank" :ui="ui">
                 <div class="flex items-center gap-5 mb-4">
                     <UIcon name="i-simple-icons-maildotru" class="text-4xl" />
                     <h1 class="text-2xl font-bold">{{ $t("contact.email.title") }}</h1>
